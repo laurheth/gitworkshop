47bbbbbb606ee5f2f0f76b4735295ea7df7fcdf5
Lauren Hetherington <hetherington@astro.utoronto.ca>
2015-09-28 16:58:44 -0400

    Why is the story important?
    It is not.

--- gitworkshop/mysillystory.py
+++ gitworkshop/mysillystory.py
@@ -1,4 +1,7 @@
 story = """Cats are always rainbow coloured.
 Tigers are never black and white.
 So if you put cats and tigers together, you always get cake pudding.
+Ways in which it is important:
+    1. Cake pudding isn't very good actually
+    2. Why am I typing this??????????????????
 """

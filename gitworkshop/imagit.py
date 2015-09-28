dc0c5378fc88189cde8e421b3b340b9e41fd237f
Lauren Hetherington <hetherington@astro.utoronto.ca>
2015-09-28 16:23:21 -0400

    Initial commit
    Basic scripts.

---
+++ gitworkshop/imagit.py
@@ -0,0 +1,7 @@
+from __future__ import print_function
+import mysillystory
+import mysillypicture
+
+if __name__ == "__main__":
+    print(mysillystory.story.encode("utf8"))
+    print(mysillypicture.picture.encode("utf8"))

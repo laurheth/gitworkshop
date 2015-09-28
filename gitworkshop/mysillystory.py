d99ed764b0bf6efd5a5d6352b21c208c8191776c
Lauren Hetherington <hetherington@astro.utoronto.ca>
2015-09-28 17:38:57 -0400

    Added another reason

--- gitworkshop/mysillystory.py
+++ gitworkshop/mysillystory.py
@@ -4,4 +4,5 @@ So if you put cats and tigers together, you always get cake pudding.
 Ways in which it is important:
     1. Cake pudding is not very good actually
     2. Why am I typing this??????????????????
+    3. Because workshop
 """
